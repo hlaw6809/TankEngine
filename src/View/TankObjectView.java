d1c8072b41b5abd7e1c947f7f82fee13bf685a77
Henry <hlaw6809@uw.edu>
2016-04-19 22:12:52 -0700

    Tank object view modification

--- src/View/TankObjectView.java
+++ src/View/TankObjectView.java
@@ -14,6 +14,8 @@ public class TankObjectView extends Canvas {
 
     TankObject tObject;
 
+    Image drawnImage;
+
     public TankObjectView(TankObject tObject) {
         super(tObject.getSize().getWidth(), tObject.getSize().getHeight());
         this.tObject = tObject;
@@ -28,10 +30,14 @@ public class TankObjectView extends Canvas {
     }
 
     public void draw() {
-        Image img = tObject.getGraphic();
-        Dimension2D size = tObject.getSize();
-        Point2D location = tObject.getLocation();
-        GraphicsContext gc = this.getGraphicsContext2D();
-        gc.drawImage(img, location.getX(), location.getY(), size.getWidth(), size.getHeight());
+        //Check for need to redraw
+        if (drawnImage == null || drawnImage != tObject.getGraphic()) {
+            Image img = tObject.getGraphic();
+            Dimension2D size = tObject.getSize();
+            Point2D location = tObject.getLocation();
+            GraphicsContext gc = this.getGraphicsContext2D();
+            gc.drawImage(img, location.getX(), location.getY(), size.getWidth(), size.getHeight());
+            drawnImage = img;
+        }
     }
 }
